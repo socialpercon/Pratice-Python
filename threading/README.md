d7a786b286eac6a3ad0e2a90671b91de53d693bf
WOO-CHEOL CHOI <socialpercon@gmail.com>
2014-03-19 15:02:32 +0900

    Timer Thread 코드 추가

---
+++ threading/README.md
@@ -0,0 +1,6 @@
+
+-- threading_timer_interval.py
+   * 어떤 함수를 일정 시간 단위로 호출 하는 코드
+
+-- threading_timer_interval_last.py
+   * 위 코드의 로직을 정리 이 코드가 가독성이 높고 확실히 동작한다.
---
+++ threading/threading_timer_interval.py
@@ -0,0 +1,42 @@
+from threading import Timer
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+def main():
+    from time import sleep
+
+    def hello(name):
+        print "Hello %s!" % name
+
+    print "starting..."
+    rt = RepeatedTimer(1, hello, "World") # it auto-starts, no need of rt.start()
+    try:
+        sleep(5) # your long-running job goes here...
+    finally:
+        rt.stop() # better in a try/finally block to make sure the program ends!
+
+if __name__ == '__main__':
+    main()
